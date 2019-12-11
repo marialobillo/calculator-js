3ac26c9ca3c45ae64fdf61becc4abf29a4f91811
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2019-12-11 17:08:44 +0100

    last details, readme file, etc

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# calculator-js
+# Calculator App
+
+_ A Calculator App using only Javascript._
+
+View live on CodePen <a href="https://codepen.io/marialobillo/pen/ExaKMwM" target="_blank">HERE</a>
+
+<br>
+
+<img src="demo.png" width="500">
+
+## Summary
+
+Calculator App is an application that allows you to make simple calculations. The main objective of this project is to serve as a learning tool and familiarize myself with Javascript language, take into functions and logic of an application.
--- app.js
+++ app.js
@@ -1,145 +1,150 @@
-const calculator = {
-  display: '0',
-  firstOperand: null,
-  waitingForSecond: false,
-  secondOperand: null, 
-  operator: null,
-}
 
-function getDigit(digit){
-  const { waitingForSecond, display } = calculator;
-
-  if(waitingForSecond == true){
-    calculator.display = digit;
-    calculator.waitingForSecond = false;
-  } else {
-    calculator.display = display === '0'? digit : display + digit;
-  }    
-}
-
-function setDecimal(dot){
-  if(calculator.waitingForSecond == true) { return;}
-
-  if(!calculator.display.includes(dot)){
-    calculator.display += dot;
+(function(){
+  const calculator = {
+    display: '0',
+    firstOperand: null,
+    waitingForSecond: false,
+    secondOperand: null, 
+    operator: null,
   }
-  updateScreen();
-}
-
-function handleOperator(operator){
-  const {firstOperand, secondOperand, display } = calculator;
-
-  if(firstOperand == null && secondOperand == null){
-
-    calculator.firstOperand = parseFloat(display);
-    calculator.display = '0';
-
-  } else if (firstOperand != null){
-
-    calculator.secondOperand = parseFloat(display);
-    let result = doCalculation(operator, firstOperand, calculator.secondOperand);
-
+  
+  function getDigit(digit){
+    const { waitingForSecond, display } = calculator;
+  
+    if(waitingForSecond == true){
+      calculator.display = digit;
+      calculator.waitingForSecond = false;
+    } else {
+      calculator.display = display === '0'? digit : display + digit;
+    }    
+  }
+  
+  function setDecimal(dot){
+    if(calculator.waitingForSecond == true) { return;}
+  
+    if(!calculator.display.includes(dot)){
+      calculator.display += dot;
+    }
+    updateScreen();
+  }
+  
+  function handleOperator(operator){
+    const {firstOperand, secondOperand, display } = calculator;
+  
+    if(firstOperand == null && secondOperand == null){
+  
+      calculator.firstOperand = parseFloat(display);
+      calculator.display = '0';
+  
+    } else if (firstOperand != null){
+  
+      calculator.secondOperand = parseFloat(display);
+      let result = doCalculation(operator, firstOperand, calculator.secondOperand);
+  
+      calculator.display = String(result);
+  
+      calculator.firstOperand = result;
+      calculator.secondOperand = null;
+      calculator.operator = null;
+      
+    }
+  
+    calculator.operator = operator;
+    calculator.waitingForSecond = true;
+  }
+  
+  function updateScreen(){
+    const display = document.querySelector('.screen');
+    display.value = calculator.display;
+  }
+  
+  
+  function doCalculation(operator, num1, num2){
+    let result;
+  
+    switch(operator){
+      case '/':
+        result = num1 / num2;
+        break;
+      case '*':
+        result = num1 * num2; 
+        break;
+      case '+': 
+        result = num1 + num2;
+        break;
+      case '-':
+        result = num1 - num2;
+        break;
+      case '=':
+        result = num2;
+        break;
+      }
+  
+      return parseFloat(result);
+  }
+  
+  function showResult(){
+    let {firstOperand, secondOperand, operator, display} = calculator;
+  
+    if(firstOperand == null || operator == null) { return;}
+    if(secondOperand != null && operator != null) { return;}
+  
+    secondOperand = parseFloat(display);
+    calculator.secondOperand = secondOperand;
+    const result = doCalculation(operator, firstOperand, secondOperand);
+    
     calculator.display = String(result);
-
-    calculator.firstOperand = result;
+    calculator.firstOperand = null;
     calculator.secondOperand = null;
     calculator.operator = null;
-    
+    calculator.waitingForSecond = false;
+    updateScreen();  
   }
   
-  calculator.operator = operator;
-  calculator.waitingForSecond = true;
-}
-
-function updateScreen(){
-  const display = document.querySelector('.screen');
-  display.value = calculator.display;
-}
-
-
-function doCalculation(operator, num1, num2){
-  let result;
-
-  switch(operator){
-    case '/':
-      result = num1 / num2;
-      break;
-    case '*':
-      result = num1 * num2; 
-      break;
-    case '+': 
-      result = num1 + num2;
-      break;
-    case '-':
-      result = num1 - num2;
-      break;
-    case '=':
-      result = num2;
-      break;
-    }
-
-    return parseFloat(result);
-}
-
-function showResult(){
-  let {firstOperand, secondOperand, operator, display} = calculator;
-
-  if(firstOperand == null || operator == null) { return;}
-  if(secondOperand != null && operator != null) { return;}
-
-  secondOperand = parseFloat(display);
-  calculator.secondOperand = secondOperand;
-  const result = doCalculation(operator, firstOperand, secondOperand);
+  function clearCalculator(){
+    calculator.display = '0';
+    calculator.firstOperand = null;
+    calculator.secondOperand = null;
+    calculator.waitingForSecondOperand = false;
+    calculator.operator =  null;
+  }
   
-  calculator.display = String(result);
-  calculator.firstOperand = null;
-  calculator.secondOperand = null;
-  calculator.operator = null;
-  calculator.waitingForSecond = false;
-  updateScreen();  
-}
-
-function clearCalculator(){
-  calculator.display = '0';
-  calculator.firstOperand = null;
-  calculator.secondOperand = null;
-  calculator.waitingForSecondOperand = false;
-  calculator.operator =  null;
-}
-
-
-updateScreen();
-
-const keys = document.querySelector('.buttons');
-  keys.addEventListener('click', (event) => {
-    const { target } = event;
+  
+  updateScreen();
+  
+  const keys = document.querySelector('.buttons');
+    keys.addEventListener('click', (event) => {
+      const { target } = event;
+      
+      if(!target.matches('button')) { return;}
     
-    if(!target.matches('button')) { return;}
-  
-    if(target.classList.contains('operator')){
-      handleOperator(target.value);
+      if(target.classList.contains('operator')){
+        handleOperator(target.value);
+        updateScreen();
+        return;
+      }
+    
+      if(target.classList.contains('decimal')){
+        setDecimal(target.value);
+        return;
+      }
+    
+      if(target.classList.contains('equal')){
+        showResult();
+        updateScreen();
+        return;
+      }
+    
+      if(target.classList.contains('clear')){
+        clearCalculator();
+        updateScreen();
+        return;
+      }
+    
+      getDigit(target.value);
       updateScreen();
-      return;
-    }
-  
-    if(target.classList.contains('decimal')){
-      setDecimal(target.value);
-      return;
-    }
-  
-    if(target.classList.contains('equal')){
-      showResult();
-      updateScreen();
-      return;
-    }
-  
-    if(target.classList.contains('clear')){
-      clearCalculator();
-      updateScreen();
-      return;
-    }
-  
-    getDigit(target.value);
-    updateScreen();
-  
-  })
+    
+    })
+
+
+})()
